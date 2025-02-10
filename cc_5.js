399c44b5456a1f33797f72f232d6e8a689464e76
bojokoeva <bojokoeva34@gmail.com>
2025-02-09 21:25:06 -0500

    Task 1

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+// Task 1: Customer Profile
+
+// Creating an object to represent a customer with some basic details
+const customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
+
+// Logging each property of the customer object
+
+console.log(`Name: ${customer.name}`);
+console.log(`Age: ${customer.age}`);
+console.log(`Email: ${customer.email}`);
